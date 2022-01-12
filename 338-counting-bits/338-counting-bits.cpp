5ae3fd99f51213dcefdaab00290a42ad1533ac73
Pankaj singh <pankaj.koranga.098@gmail.com>
2022-01-12 19:10:56 +0530

    Time: 17 ms (5.95%), Space: 7.8 MB (72.92%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> res;
+        vector<int> res(n+1,0);
         
         for(int i=0;i<=n;i++)
         {
             int n = i;
-            int count=0;
             while(n!=0)
             {
                 if(n&1)
                 {
-                    count++;
+                    res[i]++;
                 }
                 n=n>>1;
             }
-            res.push_back(count);
         }
         
         return res;
