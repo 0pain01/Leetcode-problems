65e591ef9f55cfd295744d0fe393ae528b6a6108
Pankaj singh <pankaj.koranga.098@gmail.com>
2022-01-30 10:34:04 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -11,23 +11,17 @@ class Solution
     //Function to return max value that can be put in knapsack of capacity W.
     
  
-    int util(int W,int wt[],int val[],int n,int **dp)
+    void util(int W,int wt[],int val[],int n,int **dp)
     {
-        if(n==0 || W==0)
-            return 0;
-            
-        if(dp[n][W]!=-1)
-            return dp[n][W];
         
         if(wt[n-1]<=W)
         {
-            dp[n][W] = max(val[n-1]+ util(W - wt[n-1],wt, val,n- 1, dp),util(W, wt, val,n- 1, dp));
+            dp[n][W] = max(val[n-1]+ dp[n-1][W-wt[n-1]],dp[n-1][W]);
         }
         
         else if(wt[n-1]>W)
         {
-            dp[n][W] = util(W,wt,val,n-1,dp);
-            return dp[n][W];
+            dp[n][W] = dp[n-1][W];
         }
         
     }
@@ -46,9 +40,25 @@ class Solution
             for (int j = 0; j <W+1 ; j++)
                 dp[i][j] = -1;
                 
-       return util(W,wt,val,n,dp);
+            for(int i=0;i<W+1;i++)
+            {
+                dp[0][i]=0;
+            }
+            
+            for(int i=0;i<=n;i++)
+            {
+                dp[i][0]=0;
+            }
+                
+       for(int i=1;i<n+1;i++)
+       {
+           for(int j=1;j<W+1;j++)
+           {
+               util(j,wt,val,i,dp);
+           }
+       }
        
-        
+        return dp[n][W];
     }
 };
 
