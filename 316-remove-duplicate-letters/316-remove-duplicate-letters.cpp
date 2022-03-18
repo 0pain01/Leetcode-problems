2b8dca5a951d8a5f023320175c807fbcf61a2eda
Pankaj singh <pankaj.koranga.098@gmail.com>
2022-03-18 23:12:55 +0530

    Time: 7 ms (30.82%), Space: 7.2 MB (9.60%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-         int n = s.size();
-        vector<int> lastIdx(128,0);
-        for(int i = 0 ; i < n ;i++){
-            lastIdx[s[i]] = i;
-        }
-        vector<bool> seen(128, false);
-        stack<char> st;
+         int len = s.size();
+        string res = "";
+        unordered_map<char, int> M;
+        unordered_map<char, bool> V;
+        stack<int> S;
         
-        for(int i = 0 ;i < n ; i++){
-            int curr = s[i];
-            if(seen[curr]) continue;
-            while(!st.empty() and st.top() > curr and i < lastIdx[st.top()]){
-                seen[st.top()] =  false;
-                st.pop();
+        for (auto c : s) {
+            if (M.find(c) == M.end()) M[c] = 1;
+            else M[c]++; 
+        }
+        for (unordered_map<char, int>::iterator iter=M.begin(); iter!=M.end(); iter++) V[iter->first] = false;
+        
+        cout<<M.size()<<V.size()<<endl;
+        for (int i=0; i<len; i++) {
+            M[s[i]]--;
+            if (V[s[i]] == true) continue;
+            
+            while (!S.empty() and s[i] < s[S.top()] and M[s[S.top()]] > 0) {
+                V[s[S.top()]] = false;
+                S.pop();
             }
-            st.push(curr);
-            seen[curr] = true;
+            S.push(i);
+            V[s[i]] = true;
         }
-        string ans ="";
-        while(!st.empty()){
-            ans = st.top() + ans;
-            st.pop();
+        while (!S.empty()) {
+            res = s[S.top()] + res;
+            S.pop();
         }
-        return ans;
+        return res;
     }
 };
