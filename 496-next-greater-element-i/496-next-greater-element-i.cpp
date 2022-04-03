1ddc2d0947b9d10550c9fb602fcd0db3cc0905b8
Pankaj singh <pankaj.koranga.098@gmail.com>
2022-04-04 03:45:13 +0530

    Time: 12 ms (38.64%), Space: 8.5 MB (76.95%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int search(vector<int>& nums2,int target)
+    {
+        for(int i=0;i<nums2.size();i++)
+        {
+            if(nums2[i]==target)
+                return i;
+        }
+        return 0;
+    }
+    
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>res;
+        int cnt=0;
+        for(int i=0;i<nums1.size();i++)
+        {
+            int j = search(nums2,nums1[i]);
+            cout<<j<<endl;
+         
+    
+            for(int k=j;k<nums2.size();k++)
+            {
+                if(nums2[k]>nums1[i])
+                {
+                    res.push_back(nums2[k]);
+                    cnt++;
+                    break;
+                }
+                else if(nums2[k]<=nums1[i] && k==nums2.size()-1)
+                {
+                    res.push_back(-1);
+                }
+            }
+     
+        }
+        
+        return res;
+    }
+};
