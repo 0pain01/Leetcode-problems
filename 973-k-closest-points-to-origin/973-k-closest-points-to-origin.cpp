adae8054daa0f59e78f2e18d3d479ac1d47b4437
Pankaj singh <pankaj.koranga.098@gmail.com>
2022-07-11 06:03:08 +0530

    Time: 446 ms (37.96%), Space: 67.3 MB (55.66%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    static bool solver(pair<int,double>&a, pair<int ,double>&b)
+    {
+        return (a.second<b.second);
+    }
+    
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        vector<pair<int,double>>arr;
+        int n = points.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            double ans = sqrt((points[i][0]*points[i][0])+(points[i][1]*points[i][1]));
+            arr.push_back(make_pair(i,ans));
+        }
+        
+        sort(arr.begin(),arr.end(),solver);
+        
+        for(auto x:arr)
+        {
+            cout<<x.second<<" ";
+        }
+        vector<vector<int>>ans;
+        
+        for(int i=0;i<k;i++)
+        {
+            int idx = arr[i].first;  
+            ans.push_back({points[idx][0],points[idx][1]});
+        }
+        
+        return ans;
+    }
+};
