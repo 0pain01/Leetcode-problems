2495b754a454592edcace70536b062896c60300d
Pankaj singh <pankaj.koranga.098@gmail.com>
2022-07-16 08:22:05 +0530

    Time: 18 ms (37.15%), Space: 13.1 MB (24.46%) - LeetHub

---
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countHousePlacements(int n) {
+        vector<long>res(n);
+        int mod = 1e9+7;
+        res[0]=2;
+        if(n>1)
+            res[1]=3;
+        
+        for(int i=2;i<n;i++)
+        {
+            res[i]=(res[i-1]+res[i-2])%mod;
+        }
+        int ans = (res[n-1]*res[n-1])%mod;
+        return ans;
+        
+    }
+};
