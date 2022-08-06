96b739f9c5d40bc4353ffe405780f61ca4a9c44a
Pankaj singh <pankaj.koranga.098@gmail.com>
2022-08-07 01:58:51 +0530

    Time: 87 ms (75.21%), Space: 48.2 MB (93.56%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+    int n = nums.size();
+    int max_index = 0;
+    for (int i = 0; (i < n) && (i <= max_index); i++)
+    {
+        max_index = max(max_index, i + nums[i]);
+    }
+        
+    if ((max_index >= (n - 1)))
+        return true;
+    else
+        return false;
+    }
+    
+};
