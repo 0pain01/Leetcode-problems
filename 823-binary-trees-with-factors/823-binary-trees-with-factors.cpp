0ebd38980683bfe0f211904e8716a2ae71007052
Pankaj singh <pankaj.koranga.098@gmail.com>
2022-08-10 02:31:26 +0530

    Time: 138 ms (38.77%), Space: 16.7 MB (15.69%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int numFactoredBinaryTrees(vector<int>& A) {
-        long res = 0, mod = 10e8+7;
+        long res = 0, mod = 1e9+7;
         sort(A.begin(), A.end());
         unordered_map<int, long> dp;
         for (int i = 0; i < A.size(); ++i) {
