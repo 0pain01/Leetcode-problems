5eecb500c43697c6251ca5de8f8f0c625fb57898
Pankaj singh <pankaj.koranga.098@gmail.com>
2022-08-16 22:46:55 +0530

    Time: 208 ms (24.85%), Space: 75.6 MB (5.95%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,13 +1,36 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        vector<int>dp=nums;
-        for(int i=1;i<nums.size();i++)
+    void solve(vector<int>& nums,int i,int sum,int &maxi)
+    {
+        // cout << maxi << " " << i << endl;
+        if(i==nums.size())
         {
-            dp[i]=max(dp[i],dp[i]+dp[i-1]);
-            dp[0]=max(dp[0],dp[i]);
+            maxi=max(sum,maxi);
+            return;
         }
-        return dp[0];
+        sum=sum+nums[i];
+        if(nums[i]>sum)
+        {
+            sum=nums[i];
+        }
+//         else
+//         {
+            
+//         }
+        maxi=max(sum,maxi);
+        solve(nums,i+1,sum,maxi);
+    }
+    int maxSubArray(vector<int>& nums) {
+        int maxi=INT_MIN;
+        solve(nums,0,0,maxi);
+        return maxi;
+        // vector<int>dp=nums;
+        // for(int i=1;i<nums.size();i++)
+        // {
+        //     dp[i]=max(dp[i],dp[i]+dp[i-1]);
+        //     dp[0]=max(dp[0],dp[i]);
+        // }
+        // return dp[0];
         
     }
 };
