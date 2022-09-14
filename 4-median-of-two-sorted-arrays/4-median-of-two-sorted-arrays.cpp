0a8184edc95ae06bf54293a83bbf33808bcf0c81
Pankaj singh <pankaj.koranga.098@gmail.com>
2022-09-14 20:44:53 +0530

    Time: 96 ms (9.83%), Space: 89.3 MB (63.90%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        double med=0;
-        
-        
-        if(nums1.size()==0){
-            for(int i=0;i<nums2.size();i++){
-            nums1.push_back(nums2[i]);
+        int n = nums1.size(), m = nums2.size();
+        if(n>m) return findMedianSortedArrays(nums2,nums1);
+        int midPos = (n+m+1)/2;
+        int lo = 0 ,hi = n;
+        double res = 0;
+        while(lo<=hi){
+            int cut1 = (lo+hi)>>1;
+            int cut2 = midPos - cut1;
+            int l1 = cut1==0 ? INT_MIN : nums1[cut1-1];
+            int l2 = cut2==0 ? INT_MIN : nums2[cut2-1];
+            int r1 = cut1==n ? INT_MAX : nums1[cut1];
+            int r2 = cut2==m ? INT_MAX : nums2[cut2];
+            if(l1<=r2 and l2<=r1){
+                if((n+m)%2==0){
+                    res =  (max(l1,l2) + min(r1,r2))/2.0;
+                }
+                else{
+                    res = max(l1,l2);
+                }
+                break;
+            }
+            else if(l1>r2){
+               hi =  cut1-1;
+            }
+            else{
+               lo = cut1+1;
             }
         }
-        if(nums2.size()!=0 && nums1.size()!=0){
-           
-        for(int i=0;i<nums2.size();i++){
-            nums1.push_back(nums2[i]);
-        }
-        }
-        
-        sort(nums1.begin(),nums1.end());
-        
-
-             
-        if(nums1.size()%2==0){
-             double a = nums1[(nums1.size()/2)-1];
-             double b = nums1[nums1.size()/2];
-             med= (a+b)/2;
-        }
-        else{
-            med=nums1[(nums1.size()-1)/2];
-        }
-        return med;
-        
+        return res;
     }
 };
