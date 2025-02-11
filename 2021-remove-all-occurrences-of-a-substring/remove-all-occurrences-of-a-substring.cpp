fb5f0da8c838b668feba550e89c67b3e96fd4826
Pankaj singh <pankaj.koranga.098@gmail.com>
2025-02-11 16:21:14 +0530

    Time: 7 ms (5.85%) | Memory: 13 MB (5.32%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+    int i=0;
+    while(i<s.size()){
+        if(s.substr(i,part.size())==part){
+            s.erase(i,part.size());
+            i=0;
+        }
+        else{
+            i++;
+        }
+    }    
+    return s;
+    }
+};
