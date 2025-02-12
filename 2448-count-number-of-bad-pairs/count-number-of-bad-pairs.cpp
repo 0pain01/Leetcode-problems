f67c35c0d99b8ccbfd8ff2c10c976df827966fc6
Pankaj singh <pankaj.koranga.098@gmail.com>
2025-02-12 17:03:36 +0530

    Time: 81 ms (17.82%) | Memory: 90.9 MB (26.64%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
@@ -3,17 +3,21 @@ public:
     long long countBadPairs(vector<int>& nums) {
         long long n = nums.size();
         vector<int>arr(n);
-
+        unordered_map<int,int>mp;
         for(int i=0;i<n;i++){
             arr[i]=nums[i]-i;
-        }
-        unordered_map<int,int>mp;
-
-        long long cnt =0;
-        for(int i=0;i<n;i++){
-            cnt+=mp[arr[i]];
             mp[arr[i]]++;
         }
+
+        long long cnt =0;
+
+        for(auto it:mp){
+            if(it.second>=2){
+                long long x = it.second;
+                cnt +=(x*(x-1)/2);
+            }
+        }
+
         long long total_pairs = (n*(n-1))/2;
     return  total_pairs -cnt;
     }
