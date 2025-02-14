3cc8b75fbf8d07bb587cd34b3b8163389dcc8fd7
Pankaj singh <pankaj.koranga.098@gmail.com>
2025-02-14 21:37:38 +0530

    Time: 55 ms (15.56%) | Memory: 37.3 MB (5.13%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,14 +3,14 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) { 
         map<string,vector<string>>mp1;
         for(int i=0;i<strs.size();i++){
-            map<int,int>mp2;
+            map<char,int>mp2;
             for(auto j:strs[i]){
                 mp2[j]++;
             }
             string temp;
             for(auto it:mp2)
             {
-                temp+=(to_string(it.first)+to_string(it.second));
+                temp+=(it.first+to_string(it.second));
             }
             mp1[temp].push_back(strs[i]);
             
