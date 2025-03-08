6c160475e82ab750a6ec6f4b6b5d7bd426fb066b
Pankaj singh <pankaj.koranga.098@gmail.com>
2025-03-09 01:58:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (92.34%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -9,23 +9,12 @@ public:
                 cnt++;
         }
 
-        if(cnt==k)
-            return 0;
         int mx =cnt;
 
         for(int i=k;i<n;i++){
-            if(blocks[i]=='B'){
-                if(blocks[i-k]=='W'){
-                    cnt++;
-                    mx = max(cnt,mx);
-                   
-                }
-            }
-            else{
-                if(blocks[i-k]=='B'){
-                    cnt--;
-                }
-            }
+            if (blocks[i] == 'B') cnt++;
+            if (blocks[i - k] == 'B') cnt--;
+            mx = max(mx,cnt);
         }
 
         return k-mx; 
