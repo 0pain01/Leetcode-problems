4b6f2467712668600d35fc71baf6ca5468c1896e
Pankaj singh <pankaj.koranga.098@gmail.com>
2025-04-14 23:34:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (59.99%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
-    vector<bool>vis;
     vector<int>color;
-    bool bfs(int i,vector<vector<int>>& graph){
-        queue<int>q;
-        q.push(i);
-        color[i]=0;
-        while(!q.empty()){
-            auto top = q.front();
-            q.pop();
-            vis[top]=true;
+    bool dfs(int i,int paint, vector<vector<int>>& graph){
+        if(color[i]==paint){
+            return false;
+        }
+        color[i]=paint;
 
-            for(auto it: graph[top]){
-                if(!vis[it]){
-                    if(color[it]==-1){
-                        if(color[top]==0)
-                            color[it]=1;
-                        else
-                            color[it]=0;
-                        q.push(it);
-                    }
-                    else{
-                        if(color[top]==color[it])
-                            return false;
-                        else
-                            q.push(it);
-                    }    
+        for(auto it: graph[i]){
+            if(color[it]==paint){
+                return false;
+            }
+            if(color[it]==-1){
+                if(paint==0){
+                    if(!dfs(it,1,graph))
+                        return false;
+                }
+                else{
+                    if(!dfs(it,0,graph))
+                        return false;
                 }
             }
         }
-
-        return true;
+        return true;        
     }
+
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vis.resize(n,false);
         color.resize(n,-1);
         
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                if(!bfs(i,graph))
+            if(color[i]==-1){
+                if(!dfs(i,0,graph))
                     return false;
             }
         }
