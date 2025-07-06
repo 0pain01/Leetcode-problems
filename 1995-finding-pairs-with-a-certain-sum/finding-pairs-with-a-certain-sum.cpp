188386a88b987e92f4487ff7d3a646af82f75a8b
Pankaj singh <pankaj.koranga.098@gmail.com>
2025-07-06 09:35:43 +0530

    Time: 283 ms (36.95%) | Memory: 189.8 MB (11.69%) - LeetSync

--- 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
@@ -1,36 +1,29 @@
 class FindSumPairs {
 public:
     vector<int> nums1, nums2;
-    unordered_map<int,int>mp1;
-    unordered_map<int,int>mp2;
+    unordered_map<int,int>mp;
     FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
         this->nums1 = nums1;
         this->nums2 = nums2;
 
-        for(auto i:nums1)
-            mp1[i]++;
-
         for(auto i:nums2)
-            mp2[i]++;
+            mp[i]++;
     }
     
     void add(int index, int val) {
-        mp2[nums2[index]]--;
+        mp[nums2[index]]--;
         nums2[index]+=val;
-        mp2[nums2[index]]++;
+        mp[nums2[index]]++;
     }
     
     int count(int tot) {
         int count=0;
 
-        for(auto i:mp1){
-            if(i.second>=1){
-                if(mp2[tot-i.first]>=1){
-                    count+=(i.second * mp2[tot-i.first]);
-                }
+        for(auto i:nums1){
+            if(mp[tot-i]>=1){
+                count+=mp[tot-i];
             }
         }
-
         return count;
     }
 };
