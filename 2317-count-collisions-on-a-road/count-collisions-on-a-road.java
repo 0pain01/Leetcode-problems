b85d453185b4491d7565943523e6441cc8d3c373
Pankaj singh <pankaj.koranga.098@gmail.com>
2025-12-05 00:47:00 +0530

    Time: 19 ms (39.92%) | Memory: 47.9 MB (7.26%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -3,19 +3,19 @@ class Solution {
     public int countCollisions(String directions) {
         int collisions = 0;
         int cars = directions.length();
-        char[] directionsArray = directions.toCharArray();
+        boolean prevCollision = false;
 
         int convoy = 1;
         for(int curr = 0; curr < cars; curr++) {
-            char currDir = directionsArray[curr];
+            char currDir = directions.charAt(curr);
             if(currDir == 'S') continue;
             
             int checkingIdx = currDir == 'L' ? curr - 1 : curr + 1;
             if(checkingIdx < 0 || checkingIdx >= cars) continue;
 
-            if(currDir != directionsArray[checkingIdx]) {
-                directionsArray[curr] = 'S';
+            if((checkingIdx < curr && prevCollision) || currDir != directions.charAt(checkingIdx)) {
                 collisions += convoy;
+                prevCollision = true;
                 convoy = 1;
             } else if(currDir == 'R') {
                 convoy++;
