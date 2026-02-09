3f96cf0c8206cafa94236d3c0eccd695afed0c48
Pankaj singh <pankaj.koranga.098@gmail.com>
2026-02-09 20:56:15 +0530

    Time: 2 ms (97.82%) | Memory: 48.3 MB (58.33%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
@@ -14,22 +14,22 @@
  * }
  */
 class Solution {
-    private void dfs(TreeNode root, List<TreeNode> arr){
+    private void dfs(TreeNode root, List<Integer> arr){
         if(root==null)
             return;
 
         dfs(root.left,arr);
-        arr.add(root);
+        arr.add(root.val);
         dfs(root.right,arr);
     }
 
-    private TreeNode buildTree(List<TreeNode> arr, int left, int right){
+    private TreeNode buildTree(List<Integer> arr, int left, int right){
         if(left > right)
             return null;
 
         int mid = left+(right-left)/2;
 
-        TreeNode root = arr.get(mid);
+        TreeNode root = new TreeNode(arr.get(mid));
 
         root.left = buildTree(arr,left,mid-1);
         root.right = buildTree(arr,mid+1,right);
@@ -38,7 +38,7 @@ class Solution {
     }
 
     public TreeNode balanceBST(TreeNode root) {
-        List<TreeNode> arr = new ArrayList<TreeNode> ();
+        List<Integer> arr = new ArrayList<Integer> ();
         dfs(root,arr);
         int n = arr.size();
 
