5c27d750f86ab178bc9e28240375cd052b94f07c
Pankaj singh <pankaj.koranga.098@gmail.com>
2026-02-16 21:20:00 +0530

    Time: 1 ms (60.34%) | Memory: 42.5 MB (71.14%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -2,9 +2,14 @@ class Solution {
     
     public int reverseBits(int n) {
         int res = 0;
+        //only 32 bits integer are there
         for(int i=0; i<32; i++){
+            //result left shift so that n's right side number comes to its left (in reverse order)
+            //shifting rightmost digit of n every time to get it on reverse side
             res = res<<1;
+            //extracting last digit (0 or 1)
             if((n&1)!=0) res += 1;
+            //then removing the last digit after extracting
             n = n>>1;
         }
         return res;
