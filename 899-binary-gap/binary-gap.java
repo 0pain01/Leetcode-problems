502ee4e508739bc8675e506610c7b8f220eb98de
Pankaj singh <pankaj.koranga.098@gmail.com>
2026-02-22 11:50:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (84.49%) - LeetSync

--- 899-binary-gap/binary-gap.java
+++ 899-binary-gap/binary-gap.java
@@ -8,20 +8,25 @@ class Solution {
             int digit = n & 1;
 
             if(digit == 1){
+                // starting the distance maping
                 if(!flag){
                     flag = true;
                 }
+                // ending the distance maping with including the last 1
                 else{
                     maxDistance = Math.max(maxDistance,distance + 1);
+                    // reseting for the next 1-1 mapping
                     distance = 0;
                 }
             }
             else{
+                // if the distance mapping is on then counting the distance
                 if(flag){
                     distance ++;
                 }
             }
 
+            // removing last digit with right shift
             n = n >> 1;
         }
 
