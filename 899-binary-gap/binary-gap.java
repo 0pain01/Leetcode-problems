2d8e1b5d5a6aa61c84ec9097705d6a9b33e21aed
Pankaj singh <pankaj.koranga.098@gmail.com>
2026-02-22 11:46:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (67.94%) - LeetSync

--- 899-binary-gap/binary-gap.java
+++ 899-binary-gap/binary-gap.java
@@ -6,17 +6,20 @@ class Solution {
 
         while(n > 0){
             int digit = n & 1;
-            System.out.println(digit);
-            if(digit == 1 && flag == false){
-                flag = true;
+
+            if(digit == 1){
+                if(!flag){
+                    flag = true;
+                }
+                else{
+                    maxDistance = Math.max(maxDistance,distance + 1);
+                    distance = 0;
+                }
             }
-            else if(digit == 1 && flag == true){
-                maxDistance = Math.max(maxDistance,distance + 1);
-                distance = 0;
-            }
-            
-            if(digit == 0 && flag == true){
-                distance++;
+            else{
+                if(flag){
+                    distance ++;
+                }
             }
 
             n = n >> 1;
