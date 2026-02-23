cfa333a61139f70d078b21c5e660cfea54893ac4
Pankaj singh <pankaj.koranga.098@gmail.com>
2026-02-24 02:07:20 +0530

    Time: 3 ms (46.20%) | Memory: 43.7 MB (29.58%) - LeetSync

--- 4226-check-digitorial-permutation/check-digitorial-permutation.java
+++ 4226-check-digitorial-permutation/check-digitorial-permutation.java
@@ -1,33 +1,19 @@
 class Solution {
+
+    // Precomputed factorials for 0–9
+    int[] fact = {1,1,2,6,24,120,720,5040,40320,362880};
     
     public boolean isDigitorialPermutation(int n) {
-        int n1 = n;
-        int maxDigit = 0;
         char [] numString = Integer.toString(n).toCharArray();
         Arrays.sort(numString);
 
-        while(n1 > 0){
-            maxDigit = Math.max(maxDigit, n1 % 10);
-            n1 = n1 / 10;
-        }
-
-        int [] permut = new int [maxDigit+1];
-        permut [0] = 1;
-        permut [1] = 1;
-
-        for(int i=2 ; i <= maxDigit; i++){
-            permut[i] = permut[i-1] * i;
-        }
-
         int sum = 0;
         for(int i = 0; i < numString.length; i++){
-            sum += permut[numString[i]-'0'];
+            sum += fact[numString[i]-'0'];
         }
 
         char [] permutSumString = Integer.toString(sum).toCharArray();
         Arrays.sort(permutSumString);
-
-        
 
         return Arrays.equals(numString, permutSumString);
     }
