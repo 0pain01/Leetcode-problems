a261b2df73d543f51b0bbad51ef8dacd54a48165
Pankaj singh <pankaj.koranga.098@gmail.com>
2026-04-12 23:20:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (49.18%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int zero = 0, one = 0 , two = 0;
+        int n = nums.length;
+
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0)
+                zero++;
+            else if(nums[i] == 1)
+                one++;
+            else
+                two++;
+        }
+        
+        int i = 0;
+        while(i <n){
+            while(i < n && zero > 0 ){
+                nums[i] = 0;
+                i++;
+                zero--;
+            }
+
+            while(i < n && one > 0 ){
+                nums[i] = 1;
+                i++;
+                one--;
+            }
+
+            while(i < n && two > 0 ){
+                nums[i] = 2;
+                i++;
+                two--;
+            }
+
+        }
+    
+    }
+}
