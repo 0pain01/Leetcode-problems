0194325446b9dc6839b62f694fd3d1f2d1d018a9
Pankaj singh <pankaj.koranga.098@gmail.com>
2026-06-04 21:01:37 +0530

    Time: 26 ms (72.86%) | Memory: 46.4 MB (52.04%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
@@ -2,25 +2,23 @@ class Solution {
     public int totalWaviness(int num1, int num2) {
         int count = 0;
 
-        for(int i = num1; i <= num2; i++){
-            String str = String.valueOf(i);
-            count += waveCounter(str);
+        for (int i = num1; i <= num2; i++) {
+            count += waveCounter(String.valueOf(i));
         }
 
         return count;
     }
 
-    private int waveCounter(String s){
+    private int waveCounter(String s) {
         int cnt = 0;
 
-        if(s.length() < 3)
-            return 0;
+        for (int i = 1; i < s.length() - 1; i++) {
+            int prev = s.charAt(i - 1) - '0';
+            int curr = s.charAt(i) - '0';
+            int next = s.charAt(i + 1) - '0';
 
-        for(int i = 1; i < s.length() - 1; i++){
-            if((s.charAt(i) - '0' > s.charAt(i - 1) - '0') && (s.charAt(i) - '0' > s.charAt(i + 1) - '0')){
-                cnt++;
-            }
-            else if((s.charAt(i) - '0' < s.charAt(i - 1) - '0') && (s.charAt(i) - '0' < s.charAt(i + 1) - '0')){
+            if ((curr > prev && curr > next) ||
+                (curr < prev && curr < next)) {
                 cnt++;
             }
         }
