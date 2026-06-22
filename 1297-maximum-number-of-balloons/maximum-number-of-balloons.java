83fb0248d6bd8ac99459c975c8b394b1c663cd4c
Pankaj singh <pankaj.koranga.098@gmail.com>
2026-06-23 00:49:06 +0530

    Time: 8 ms (49.83%) | Memory: 43.4 MB (71.44%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -3,23 +3,21 @@ class Solution {
         int b = 0, l = 0, a = 0, o = 0, n = 0;
 
         for (int i = 0; i < text.length(); i++){
-            if(text.charAt(i)=='b'){
+            if(text.charAt(i)=='b') 
                 b++;
-            }else if(text.charAt(i)=='a'){
+            else if(text.charAt(i)=='a')    
                 a++;
-            }else if(text.charAt(i)=='l'){
+            else if(text.charAt(i)=='l')
                 l++;
-            }else if(text.charAt(i)=='o'){
+            else if(text.charAt(i)=='o')
                 o++;
-            }else if(text.charAt(i)=='n'){
+            else if(text.charAt(i)=='n')
                 n++;
-            }
         }
 
         l = l % 2 == 0 ? l/2 : (l-1)/2;
         o = o % 2 == 0 ? o/2 : (o-1)/2;
 
-        int ans = Math.min(b,(Math.min(a,Math.min(l,Math.min(o,n)))));
-        return ans;
+        return Math.min(b,(Math.min(a,Math.min(l,Math.min(o,n)))));
     }
 }
